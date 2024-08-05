5d53a8a710c4bfd7b85a13b55658270d5d463b24
Brendon <bluicien@yahoo.com>
2024-08-05 22:03:23 +0900

    Added script for spinning icons individually

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// COPY PASTA
+
 let slider = document.querySelector(".skills-flex");
 let innerSlider = document.querySelector(".skills-list");
 
@@ -17,7 +19,7 @@ slider.addEventListener("mousedown", (e) => {
   slider.style.cursor = "grabbing";
 });
 
-slider.addEventListener("mouseenter", () => {
+slider.addEventListener("mouseenter", (e) => {
   slider.style.cursor = "grab";
 });
 
@@ -32,13 +34,13 @@ window.addEventListener("mouseup", () => {
 slider.addEventListener("mousemove", (e) => {
   if (!pressed) return;
   e.preventDefault();
-
   x = e.offsetX;
   console.log(`x: ${x}, startx: ${startx}`);
   console.log(`Diff: ${x - startx}`);
 
   innerSlider.style.left = `${x - startx}px`;
   checkBoundary();
+
 });
 
 function checkBoundary(x, startx) {
@@ -50,5 +52,44 @@ function checkBoundary(x, startx) {
   }
   if (inner.right < outer.right) {
     innerSlider.style.left = `-${inner.width - outer.width}px`;
+  }
+}
+
+// MY SCRIPTS
+
+// Run spin icon function when mouse hovers over slider
+slider.addEventListener("mousemove", (e) => {
+  spinIcon(e);
+});
+
+// Function gets children of the slider and runs animation on the child
+// when mouse hovers over it
+function spinIcon(event) {
+  let sliderChildren = innerSlider.getElementsByClassName("slide-skills")
+  
+  for (let i = 0; i < sliderChildren.length; i++) {
+    let iconPosition = sliderChildren[i].getBoundingClientRect();
+    if (checkHover(event, iconPosition)) {
+      // console.log(`Your mouse is at ${e.clientX}-X and ${e.clientY}-Y`)
+      sliderChildren[i].children[0].style.animation = "rotate-logo 8s ease-in-out 0s";
+    }
+  }
+}
+
+// Function takes 2 arguments, mouse event and element position from getBoundingClientRect()
+// and de-structures them. Then checks if the mouse is hovering over the element
+function checkHover ({clientX, clientY}, {x, y, width, height}) {
+  let rightSideX = x + width;
+  let bottomSideY = y + height;
+
+  if (
+    (clientX + 1)>= x &&
+    (clientX - 1) <= rightSideX &&
+    (clientY + 1)>= y &&
+    (clientY - 1) <= bottomSideY
+  ) {
+    return true;
+  } else {
+    return false;
   }
 }
