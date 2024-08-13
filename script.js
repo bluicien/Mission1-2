bfad852efc95701aa4e42bf2a913f996e9934b70
Brendon Luicien <brendonl@missionreadyhq.com>
2024-08-13 21:19:16 +1200

    No changes

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ let pressed = false;
 let startx;
 let x;
 
-console.log(slider)
-console.log(innerSlider)
+console.log(slider);
+console.log(innerSlider);
+
+const soundBtn = document.getElementById("sound-control"); // Mute/Unmute button
+console.log(soundBtn.style);
 
 slider.addEventListener("mousedown", (e) => {
   pressed = true;
@@ -40,7 +43,6 @@ slider.addEventListener("mousemove", (e) => {
 
   innerSlider.style.left = `${x - startx}px`;
   checkBoundary();
-
 });
 
 function checkBoundary(x, startx) {
@@ -55,7 +57,7 @@ function checkBoundary(x, startx) {
   }
 }
 
-// MY SCRIPTS
+// My Own Scripts
 
 // Run spin icon function when mouse hovers over slider
 slider.addEventListener("mousemove", (e) => {
@@ -65,30 +67,30 @@ slider.addEventListener("mousemove", (e) => {
 // Function gets children of the slider and runs animation on the child
 // when mouse hovers over it
 function spinIcon(event) {
-  let sliderChildren = innerSlider.getElementsByClassName("slide-skills")
-  
+  let sliderChildren = innerSlider.getElementsByClassName("slide-skills");
+
   for (let i = 0; i < sliderChildren.length; i++) {
     let iconPosition = sliderChildren[i].getBoundingClientRect();
     if (checkHover(event, iconPosition)) {
       // console.log(`Your mouse is at ${e.clientX}-X and ${e.clientY}-Y`)
-      sliderChildren[i].children[0].style.animation = "rotate-logo 8s ease-in-out 0s";
+      sliderChildren[i].children[0].style.animation =
+        "rotate-logo 8s ease-in-out 0s";
     }
   }
 }
 
 // Function takes 2 arguments, mouse event and element position from getBoundingClientRect()
 // and de-structures them. Then checks if the mouse is hovering over the element
-function checkHover ({clientX, clientY}, {x, y, width, height}) {
-
+function checkHover({ clientX, clientY }, { x, y, width, height }) {
   let rightSideX = x + width; // X coord for right side of the element
   let bottomSideY = y + height; // Y coord for bottom side of the element
 
   // Check if mouse is within the coords of the element and return true if it is
   if (
-    (clientX + 1)>= x &&
-    (clientX - 1) <= rightSideX &&
-    (clientY + 1)>= y &&
-    (clientY - 1) <= bottomSideY
+    clientX + 1 >= x &&
+    clientX - 1 <= rightSideX &&
+    clientY + 1 >= y &&
+    clientY - 1 <= bottomSideY
   ) {
     return true;
   } else {
@@ -101,8 +103,8 @@ function soundChange() {
   const soundBtn = document.getElementById("sound-control"); // Mute/Unmute button
   const audioPlayer = document.getElementById("audio-player"); // audio html for background music
 
-  const mute = '<i class="fa-solid fa-volume-xmark fa-xl"></i>' // Muted icon
-  const soundOn = '<i class="fa-solid fa-volume-high fa-xl"></i>' // Sound icon
+  const mute = '<i class="fa-solid fa-volume-xmark fa-xl"></i>'; // Muted icon
+  const soundOn = '<i class="fa-solid fa-volume-high fa-xl"></i>'; // Sound icon
 
   // Switch mute on/off based on current state
   audioPlayer.muted = !audioPlayer.muted;
@@ -110,14 +112,14 @@ function soundChange() {
   // If muted, unmute and change style
   if (audioPlayer.muted) {
     soundBtn.innerHTML = mute;
-    soundBtn.style.backgroundColor = "white"
-    soundBtn.style.color = "#333"
-  } 
+    soundBtn.style.backgroundColor = "white";
+    soundBtn.style.color = "#333";
+  }
   // If unmuted, mute and change style
   else {
-    soundBtn.innerHTML = soundOn
-    soundBtn.style.backgroundColor = "#333"
-    soundBtn.style.color = "white"
+    soundBtn.innerHTML = soundOn;
+    soundBtn.style.backgroundColor = "#333";
+    soundBtn.style.color = "white";
   }
-  return;
+  console.log(soundBtn.style);
 }
